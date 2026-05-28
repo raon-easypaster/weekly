b4cf6f70355f3ea79cbf069619ee65eb9695e2e4
raon-easypaster <233180327+raon-easypaster@users.noreply.github.com>
2026-05-28 10:10:23 +0000

    Auto-update archive data [skip ci]

--- data/archiveData.js
+++ data/archiveData.js
@@ -2,9 +2,9 @@ window.ARCHIVE_DATA = [
   {
     "title": "260524 출애굽기 12장 21절a 모세의 이르되 (모세 9)",
     "scripture": "출애굽기 12:21a",
-    "date": "2026-05-23",
-    "url": "2026/260523daily.html",
-    "fileName": "260523daily.html"
+    "date": "2026-05-24",
+    "url": "2026/260524daily.html",
+    "fileName": "260524daily.html"
   },
   {
     "title": "모세의 지팡이 (모세 8)",
